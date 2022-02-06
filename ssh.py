b8f803e4348e54a68e99cde07a29bad188acb24c
R00tendo <72181445+R00tendo@users.noreply.github.com>
2022-02-06 04:53:34 -0500

    Delete ssh.py

--- ssh.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-host = "127.0.0.1"
-username = "kali"
-password = "192.168"
-sh = paramiko.SSHClient()
-sh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-sh.connect(host, 22, username, password)
-sh.close()
-print("Done")
