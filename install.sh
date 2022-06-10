cd90b4f9034737cae77571de25e0fba96004a9ae
R00tendo <72181445+R00tendo@users.noreply.github.com>
2022-06-10 10:26:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,11 @@ pip3 install termcolor
 pip3 install ftplib
 pip3 install paramiko
 sudo apt-get install libmariadb3 libmariadb-dev
+apt install golang
 pip3 install mariadb
 pip3 install smbprotocol
 pip3 install telnetlib
-apt install assetfinder
-wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
-chmod +x findomain-linux
-cp findomain-linux /usr/bin/findomain
-git clone https://github.com/signedsecurity/sigurlfind3r.git &&
-cd sigurlfind3r/cmd/sigurlfind3r/ &&
-go build; mv sigurlfind3r /usr/local/bin/
+go get -u github.com/tomnomnom/assetfinder
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux && chmod +x findomain-linux && cp findomain-linux /usr/bin/findomain
+git clone https://github.com/signedsecurity/sigurlfind3r.git && cd sigurlfind3r/cmd/sigurlfind3r/ && go build; mv sigurlfind3r /usr/local/bin/
 echo "SSB INSTALLED!"
