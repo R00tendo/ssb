bb01f2cc3d7d9424009d4f9b119d459bf155b0ab
R00tendo <72181445+R00tendo@users.noreply.github.com>
2022-06-10 09:49:42 +0000

    Installation script :D

---
+++ install.sh
@@ -0,0 +1,16 @@
+pip3 install requests
+pip3 install termcolor
+pip3 install ftplib
+pip3 install paramiko
+sudo apt-get install libmariadb3 libmariadb-dev
+pip3 install mariadb
+pip3 install smbprotocol
+pip3 install telnetlib
+apt install assetfinder
+wget https://github.com/findomain/findomain/releases/latest/download/findomain-linux
+chmod +x findomain-linux
+cp findomain-linux /usr/bin/findomain
+git clone https://github.com/signedsecurity/sigurlfind3r.git &&
+cd sigurlfind3r/cmd/sigurlfind3r/ &&
+go build; mv sigurlfind3r /usr/local/bin/
+echo "SSB INSTALLED!"
